8a89fc80b471c8feac5af9a514165393a497e624
Laurex13 <pritiriti2013@gmail.com>
2021-06-17 14:48:53 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/laurita_charles/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.laurita_charles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
